278761d58ea515ab0d9ea5cd6c7871365b6a1626
Mina176 <mtrmena176@gmail.com>
2026-07-17 18:38:45 +0300

    navigate to details view

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,11 +1,25 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import { ListItemBase$PressEvent } from "sap/m/ListItemBase";
+import UIComponent from "sap/ui/core/UIComponent";
+import Context from "sap/ui/model/Context";
 
 /**
  * @namespace com.myorg.myapp.controller
  */
 export default class Main extends BaseController {
-	public sayHello(): void {
-		MessageBox.show("Hello World!");
+	onStandardListItemPress(oEvent: ListItemBase$PressEvent) {
+		const pressedItem = oEvent.getSource();
+		const context = pressedItem.getBindingContext("employeeModel") as Context;
+		const desiredPath = (context).getPath() as string;
+		// /employees/0
+		const cleanPath = (desiredPath).substring(1);
+		// employees/0
+		const encodedPath = window.encodeURIComponent(cleanPath)
+		// employees%2F0
+		const router = UIComponent.getRouterFor(this);
+		router.navTo("detail", {
+			empId: encodedPath
+		});
 	}
 }
