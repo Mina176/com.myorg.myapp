b7ad43a1e8506ccb3cba96a9399b1d364b172fa8
Mina176 <mtrmena176@gmail.com>
2026-07-17 19:03:38 +0300

    add comments in detail controller

--- webapp/controller/Detail.controller.ts
+++ webapp/controller/Detail.controller.ts
@@ -10,11 +10,17 @@ export default class Detail extends Controller {
     onInit(): void {
         const router = UIComponent.getRouterFor(this);
         (router.getRoute("detail") as Route).attachPatternMatched(this.onObjectMatched, this);
+        // looks inside manifest.json in routes a route named "detail" (manifest.json/routes/name:detail)
     }
-
+    // this method executes everytime user lands on this page 
     onObjectMatched(event: Route$PatternMatchedEvent): void {
+        const encodedPath = (event.getParameter("arguments") as any).empId
+        // employees%2F0
+        const decodedPath = window.decodeURIComponent(encodedPath);
+        // employees/0
         this.getView()?.bindElement({
-            path: "/" + window.decodeURIComponent((event.getParameter("arguments") as any).empId),
+            path: "/" + decodedPath,
+            // /employees/0
             model: "employeeModel"
         });
     }
